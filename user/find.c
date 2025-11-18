182700b354391e603ac89b85d706abc36371dc80
KhaledAMagdi <khaledmadgdi@gmail.com~git>
2025-11-18 14:37:17 +0200

    implementation of find

--- user/find.c
+++ user/find.c
@@ -2,15 +2,39 @@
 #include "kernel/types.h"
 #include "user/user.h"
 
+#include "kernel/fs.h"
+#include "kernel/stat.h"
+
+void print_stat(struct stat *st) {
+    printf("dev:   %d\n", st->dev);
+    printf("ino:   %d\n", st->ino);
+    printf("type:  %d\n", st->type);
+    printf("nlink: %d\n", st->nlink);
+    printf("size:  %lu\n", st->size);  // <<< fix
+}
+
+
 int main(int argc, char *argv[]) {
     int fd;
+    char *path = argv[1];
+    struct dirent de;
+    struct stat st;
 
-    if ((fd = open(argv[1], 0)) < 0) {
-        printf("find: cannot open %s\n", argv[1]);
+    if ((fd = open(path, 0)) < 0) {
+        printf("find: cannot open %s\n", path);
         exit(0);
     } else {
-        printf("find: file %s opened\n", argv[1]);
+        printf("find: file %s opened\n", path);
+
+        if (fstat(fd, &st) < 0) {
+            printf("find: cannot stat %s\n", path);
+            close(fd);
+            exit(0);
+        }
+
+        print_stat(&st);
         close(fd);
+
         exit(0);
     }
 }
