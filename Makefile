7b81498b036e9fd16344c073f28f6334842bb0b2
KhaledAMagdi <khaledmadgdi@gmail.com~git>
2025-11-18 22:40:05 +0200

    mv command implementation

--- Makefile
+++ Makefile
@@ -126,6 +126,7 @@ UPROGS=\
 	$U/_cat\
 	$U/_echo\
 	$U/_find\
+	$U/_mv\
 	$U/_forktest\
 	$U/_grep\
 	$U/_init\
---
+++ user/mv.c
@@ -0,0 +1,26 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+int main(int argc, char* argv[])
+{
+  if (argc != 3 || (argc == 2 && strcmp(argv[1],"?") == 0)) {
+        printf("Usage: mv <filenameDirectory_old> <filenameDirectory_new>\n");
+        exit(0);
+    }
+
+    char* old = argv[1];
+    char* new = argv[2];
+
+    if(link(old, new) < 0){
+      printf("mv: link %s %s failed\n",old, new);
+      exit(0);
+    }
+
+    if(unlink(old) < 0){
+      printf("mv: unlink %s failed\n",old);
+      exit(0);
+    }
+
+    exit(0);
+}
